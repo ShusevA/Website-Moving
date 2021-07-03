ae4607fe77da24424d7c82fb6208394cdd704c90
ShusevA <60173266+ShusevA@users.noreply.github.com>
2021-07-03 11:35:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+let scrollPos = 0;
+
+$(document).ready(function () {
+
+    const delta = $('.effect').offset();
+
+    $(document).scroll(function () {
+        let scrolling = $(this).scrollTop();
+        if (scrolling > scrollPos) {
+            $('.photo').css({ animationName: "mymove", animationDuration: "1s" });
+        } else {
+            $('.photo').css({ animationName: "backMymove", animationDuration: "1s" });
+            setTimeout(() => {
+                $('.photo').css({ animationName: "mymove" });
+            }, 1000);
+        }
+        $('.photo').animate({ animationDuration: "30s" }, 1);
+        scrollPos = scrolling;
+    });
+
+    $(".main").mousemove(function (e) {
+        let allPage = $('.main').innerWidth() / 2;
+        let allElem = delta.left / 1.3;
+        let muvRight = parseInt(e.pageX - $('.effect').width());
+        if (e.pageX > allPage) {
+            if (muvRight < allElem) return;
+            $(".effect").animate({ marginLeft: muvRight + 40 }, 300);
+        } else {
+            if (e.pageX > (allElem + 40)) return;
+            $(".effect").animate({ marginLeft: e.pageX - 70 }, 300);
+        }
+    });
+
+    $(document).on("click", ".btn", () => {
+        $('.language__all').toggle();
+    });
+
+});
+
+
+
+
+
+
+
+
